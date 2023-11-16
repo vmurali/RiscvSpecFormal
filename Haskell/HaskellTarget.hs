61dc3503f59c362127e8d21a829cb7095dc3f621
Murali Vijayaraghavan <muralivi@google.com>
2023-11-16 00:17:34 -0800

    updated tutorial

--- Haskell/HaskellTarget.hs
+++ Haskell/HaskellTarget.hs
@@ -1,8 +1,9 @@
-module HaskellTarget (module Syntax, module Word, module Fin, module EclecticLib, module PeanoNat, module Test, module NativeTest, module Instance) where
+module HaskellTarget (module Syntax, module Word, module Fin, module EclecticLib, module PeanoNat, module Test, module NativeTest, module Instance , module ExtractEx ) where
 import EclecticLib hiding (__)
 import PeanoNat
 import Fin
 import Instance hiding (unsafeCoerce, Any)
+import ExtractEx hiding (unsafeCoerce, Any)
 import Syntax hiding (__)
 import Word
 import Test hiding (unsafeCoerce, __, counter)
--- Haskell/Target.hs
+++ Haskell/Target.hs
@@ -1,9 +1,10 @@
-module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, module CompilerSimple, module Extraction, module LibStruct, module Instance, module Test, rtlMod) where
+module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, module CompilerSimple, module Extraction, module LibStruct, module Instance, module ExtractEx, module Test, rtlMod) where
 import CompilerSimple hiding (unsafeCoerce, __)
 import EclecticLib hiding (__)
 import PeanoNat
 import Fin
 import Instance hiding (unsafeCoerce, __)
+import ExtractEx hiding (unsafeCoerce, __)
 import Rtl
 import Syntax hiding (__)
 import Extraction hiding (unsafeCoerce, Any)
