96f65c345d00bf1ff29da1615021510da8a5c297
Murali Vijayaraghavan <muralivi@google.com>
2024-10-04 08:00:11 -0700

    LibStruct to LibMisc

--- Haskell/Target.hs
+++ Haskell/Target.hs
@@ -1,4 +1,4 @@
-module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, module CompilerSimple, module Extraction, module LibStruct, module Instance, module ExtractEx, module Test, rtlMod) where
+module Target (module Syntax, module Rtl, module Word, module Fin, module EclecticLib, module PeanoNat, module CompilerSimple, module Extraction, module LibMisc, module Instance, module ExtractEx, module Test, rtlMod) where
 import CompilerSimple hiding (unsafeCoerce, __)
 import EclecticLib hiding (__)
 import PeanoNat
@@ -9,7 +9,7 @@ import Rtl
 import Syntax hiding (__)
 import Extraction hiding (unsafeCoerce, Any)
 import Word
-import LibStruct hiding (unsafeCoerce, __)
+import LibMisc hiding (unsafeCoerce, __)
 import Test hiding (unsafeCoerce, Any)
 
 rtlMod :: ([String], ([RegFileBase], BaseModule))
